7b689e5b2efcd633c23faa87cfbac4c99e8e0237
Ian Terrell <ian.terrell@gmail.com>
2014-10-02 14:45:42 -0700

    Make "Hello, world." a very simple /proxy/ server.

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world.\n")
-}
+var (
+	listen = flag.String("listen", ":8080", "address to listen on")
+)
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", proxyHandlerFunc)
+	log.Fatal(http.ListenAndServe(*listen, nil))
+}
+
+func proxyHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	resp, err := client.Get("http://localhost:4567")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, string(body))
 }
---
+++ test/servers/helloworld.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+get '/' do
+  "Hello, world!\n"
+end
