6e51aa6732d11f8a6b87e6d2c017e8c4f154afca
Ian Terrell <ian.terrell@gmail.com>
2014-10-02 15:52:21 -0700

    Manipulate request and response bodies in transit.

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/robertkrimen/otto"
 )
 
 var (
@@ -18,6 +21,12 @@ func main() {
 }
 
 func proxyHandlerFunc(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	client := &http.Client{}
 
 	req, err := http.NewRequest(r.Method, "http://localhost:4567", nil)
@@ -25,14 +34,41 @@ func proxyHandlerFunc(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	req.Body = r.Body
+	req.Body = ioutil.NopCloser(massageBody(bytes.NewBuffer(body), `
+		body = body.replace("Stan", "Kyle");
+	`))
 
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Fprint(w, string(body))
+	defer resp.Body.Close()
+	newBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newRespBody := massageBody(bytes.NewBuffer(newBody), `
+		body = body.replace("Hello", "Bye bye");
+	`)
+
+	fmt.Fprint(w, newRespBody.String())
+}
+
+func massageBody(body *bytes.Buffer, src interface{}) *bytes.Buffer {
+	vm := otto.New()
+	vm.Set("body", body.String())
+	vm.Run(src)
+	newBodyRaw, err := vm.Get("body")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newBody, err := newBodyRaw.ToString()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return bytes.NewBufferString(newBody)
 }
