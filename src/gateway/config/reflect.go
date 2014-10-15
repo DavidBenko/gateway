8478b43c3b97b9976514b759ffb21c50658866c4
Ian Terrell <ian.terrell@gmail.com>
2014-10-15 16:57:45 -0700

    DRY up configuration reflection.

--- src/gateway/config/reflect.go
+++ src/gateway/config/reflect.go
@@ -7,9 +7,81 @@ import (
 	"strconv"
 )
 
-// These could be DRY'd up, I'm sure.
-
 func setupFlags(value reflect.Value) {
+	reflectConfiguration(
+		value,
+		func(flagName, defaultValue, flagUsage string) bool {
+			return flagName != ""
+		},
+		func(fieldValue reflect.Value, flagName, flagValue, flagUsage string) {
+			switch fieldValue.Kind() {
+			case reflect.Int64:
+				intValue, err := strconv.ParseInt(flagValue, 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+				flag.Int64(flagName, intValue, flagUsage)
+			case reflect.String:
+				flag.String(flagName, flagValue, flagUsage)
+			}
+		},
+	)
+}
+
+func setDefaults(value reflect.Value) {
+	reflectConfiguration(
+		value,
+		func(flagName, defaultValue, flagUsage string) bool {
+			return defaultValue != ""
+		},
+		func(fieldValue reflect.Value, flagName, defaultValue, flagUsage string) {
+			switch fieldValue.Kind() {
+			case reflect.Int64:
+				flagInt, err := strconv.ParseInt(defaultValue, 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fieldValue.SetInt(flagInt)
+			case reflect.String:
+				fieldValue.SetString(defaultValue)
+			}
+		},
+	)
+}
+
+func setFromFlags(value reflect.Value) {
+	setFlags := make(map[string]flag.Value)
+	flag.Visit(func(f *flag.Flag) {
+		setFlags[f.Name] = f.Value
+	})
+
+	reflectConfiguration(
+		value,
+		func(flagName, flagValue, flagUsage string) bool {
+			_, ok := setFlags[flagName]
+			return ok
+		},
+		func(fieldValue reflect.Value, flagName, flagValue, flagUsage string) {
+			setFlagValue, _ := setFlags[flagName]
+			switch fieldValue.Kind() {
+			case reflect.Int64:
+				flagInt, err := strconv.ParseInt(setFlagValue.String(), 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fieldValue.SetInt(flagInt)
+			case reflect.String:
+				fieldValue.SetString(setFlagValue.String())
+			}
+		},
+	)
+}
+
+func reflectConfiguration(
+	value reflect.Value,
+	shouldHandle func(flagName, flagValue, flagUsage string) bool,
+	handle func(fieldValue reflect.Value, flagName, flagValue, flagUsage string),
+) {
 	if value.Kind() == reflect.Struct {
 		t := reflect.TypeOf(value.Interface())
 		n := t.NumField()
@@ -22,76 +94,10 @@ func setupFlags(value reflect.Value) {
 
 			fieldValue := value.FieldByName(field.Name)
 
-			if flagName != "" {
-				switch fieldValue.Kind() {
-				case reflect.Int64:
-					intValue, err := strconv.ParseInt(flagValue, 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					flag.Int64(flagName, intValue, flagUsage)
-				case reflect.String:
-					flag.String(flagName, flagValue, flagUsage)
-				}
+			if shouldHandle(flagName, flagValue, flagUsage) {
+				handle(fieldValue, flagName, flagValue, flagUsage)
 			} else if fieldValue.Kind() == reflect.Struct {
-				setupFlags(fieldValue)
-			}
-		}
-	}
-}
-
-func setDefaults(value reflect.Value) {
-	if value.Kind() == reflect.Struct {
-		t := reflect.TypeOf(value.Interface())
-		n := t.NumField()
-		for i := 0; i < n; i++ {
-			field := t.Field(i)
-			defaultValue := field.Tag.Get("default")
-			fieldValue := value.FieldByName(field.Name)
-			if defaultValue != "" {
-				switch fieldValue.Kind() {
-				case reflect.Int64:
-					flagInt, err := strconv.ParseInt(defaultValue, 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					fieldValue.SetInt(flagInt)
-				case reflect.String:
-					fieldValue.SetString(defaultValue)
-				}
-			} else if fieldValue.Kind() == reflect.Struct {
-				setDefaults(fieldValue)
-			}
-		}
-	}
-}
-
-func setFromFlags(value reflect.Value) {
-	setFlags := make(map[string]flag.Value)
-	flag.Visit(func(f *flag.Flag) {
-		setFlags[f.Name] = f.Value
-	})
-
-	if value.Kind() == reflect.Struct {
-		t := reflect.TypeOf(value.Interface())
-		n := t.NumField()
-		for i := 0; i < n; i++ {
-			field := t.Field(i)
-			flag := field.Tag.Get("flag")
-			fieldValue := value.FieldByName(field.Name)
-			if setFlagValue, ok := setFlags[flag]; ok {
-				switch fieldValue.Kind() {
-				case reflect.Int64:
-					flagInt, err := strconv.ParseInt(setFlagValue.String(), 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					fieldValue.SetInt(flagInt)
-				case reflect.String:
-					fieldValue.SetString(setFlagValue.String())
-				}
-			} else if fieldValue.Kind() == reflect.Struct {
-				setFromFlags(fieldValue)
+				reflectConfiguration(fieldValue, shouldHandle, handle)
 			}
 		}
 	}
