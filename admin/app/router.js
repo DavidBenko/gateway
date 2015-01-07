694495ae5e438392974d3bb719806c547e3e45fb
Ian Terrell <ian.terrell@gmail.com>
2015-01-07 14:15:53 -0800

    Update to new Handlebars system and start migrating.

--- admin/app/router.js
+++ admin/app/router.js
@@ -6,6 +6,23 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('admin', function() {
+    this.resource('routes');
+    this.resource('endpoints', function() {
+      this.resource('newEndpoint', { path: 'new' });
+      this.resource('endpoint', { path: ':endpoint_id' });
+    });
+    this.resource('libraries', function() {
+      this.resource('newLibrary', { path: 'new' });
+      this.resource('library', { path: ':library_id' });
+    });
+    this.resource('environments', function() {
+      this.resource('newEnvironment', { path: 'new' });
+      this.resource('environment', { path: ':environment_id' });
+    });
+  });
+  this.resource('docs');
+  this.resource('support');
 });
 
 export default Router;
