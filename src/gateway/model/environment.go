f469ea148cae425ca559f2553da79dbe2c35c15f
Ian Terrell <ian.terrell@gmail.com>
2015-01-19 13:36:49 -0800

    Update environments to contain arbitrary JSON.

--- src/gateway/model/environment.go
+++ src/gateway/model/environment.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"encoding/json"
 	"fmt"
 	"gateway/config"
 	apsql "gateway/sql"
@@ -12,9 +13,10 @@ type Environment struct {
 	AccountID int64 `json:"-"`
 	APIID     int64 `json:"-" db:"api_id"`
 
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID          int64           `json:"id"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Data        json.RawMessage `json:"data"`
 }
 
 // Validate validates the model.
@@ -33,7 +35,8 @@ func AllEnvironmentsForAPIIDAndAccountID(db *apsql.DB, apiID, accountID int64) (
 		"SELECT "+
 			"  `environments`.`id` as `id`, "+
 			"  `environments`.`name` as `name`, "+
-			"  `environments`.`description` as `description` "+
+			"  `environments`.`description` as `description`, "+
+			"  `environments`.`data` as `data` "+
 			"FROM `environments`, `apis` "+
 			"WHERE `environments`.`api_id` = ? "+
 			"  AND `environments`.`api_id` = `apis`.`id` "+
@@ -51,6 +54,7 @@ func FindEnvironmentForAPIIDAndAccountID(db *apsql.DB, id, apiID, accountID int6
 			"  `environments`.`id` as `id`, "+
 			"  `environments`.`name` as `name`, "+
 			"  `environments`.`description` as `description` "+
+			"  `environments`.`data` as `data` "+
 			"FROM `environments`, `apis` "+
 			"WHERE `environments`.`id` = ? "+
 			"  AND `environments`.`api_id` = ? "+
@@ -82,12 +86,16 @@ func DeleteEnvironmentForAPIIDAndAccountID(tx *apsql.Tx, id, apiID, accountID in
 
 // Insert inserts the environment into the database as a new row.
 func (e *Environment) Insert(tx *apsql.Tx) error {
+	data, err := e.Data.MarshalJSON()
+	if err != nil {
+		return err
+	}
 	result, err := tx.Exec(
-		"INSERT INTO `environments` (`api_id`, `name`, `description`) "+
+		"INSERT INTO `environments` (`api_id`, `name`, `description`, `data`) "+
 			"VALUES ( "+
 			"  (SELECT `id` FROM `apis` WHERE `id` = ? AND `account_id` = ?), "+
-			"  ?, ?);",
-		e.APIID, e.AccountID, e.Name, e.Description)
+			"  ?, ?, ?);",
+		e.APIID, e.AccountID, e.Name, e.Description, string(data))
 	if err != nil {
 		return err
 	}
@@ -102,13 +110,17 @@ func (e *Environment) Insert(tx *apsql.Tx) error {
 
 // Update updates the environment in the database.
 func (e *Environment) Update(tx *apsql.Tx) error {
+	data, err := e.Data.MarshalJSON()
+	if err != nil {
+		return err
+	}
 	result, err := tx.Exec(
 		"UPDATE `environments` "+
-			"SET `name` = ?, `description` = ? "+
+			"SET `name` = ?, `description` = ?, `data` = ? "+
 			"WHERE `environments`.`id` = ? "+
 			"  AND `environments`.`api_id` IN "+
 			"      (SELECT `id` FROM `apis` WHERE `id` = ? AND `account_id` = ?)",
-		e.Name, e.Description, e.ID, e.APIID, e.AccountID)
+		e.Name, e.Description, string(data), e.ID, e.APIID, e.AccountID)
 	if err != nil {
 		return err
 	}
--- src/gateway/sql/static/sqlite3/v1/create_environments.sql
+++ src/gateway/sql/static/sqlite3/v1/create_environments.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `environments` (
   `api_id` INTEGER NOT NULL,
   `name` TEXT NOT NULL,
   `description` TEXT,
+  `data` TEXT NOT NULL,
   FOREIGN KEY(`api_id`) REFERENCES `apis`(`id`) ON DELETE CASCADE
 );
