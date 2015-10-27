dd6ee0bcca48a25d0b025dd7fe3439b143a56f42
Bodie Solomon <bodie@synapsegarden.net>
2015-10-27 13:16:43 -0400

    [#89625966] No longer SET NULL on ProxyEndpointComponent FK constraint

--- src/gateway/sql/static/postgres/v4/alter_proxy_endpoint_components.sql
+++ src/gateway/sql/static/postgres/v4/alter_proxy_endpoint_components.sql
@@ -1,5 +1,4 @@
 ALTER TABLE proxy_endpoint_components
   ADD COLUMN shared_component_id INTEGER,
-  ADD FOREIGN KEY("shared_component_id")
-    REFERENCES "shared_components"("id")
-    ON DELETE SET NULL;
+  ADD FOREIGN KEY "shared_component_id"
+    REFERENCES "shared_components"("id");
