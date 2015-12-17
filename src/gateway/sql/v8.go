d335f78397941c67f0f05ea23d9c91c856188e46
Rick Snyder <rsnyder@anypresence.com>
2015-12-17 12:17:25 -0500

    [#110254764] Fix borked tests

--- src/gateway/sql/v8.go
+++ src/gateway/sql/v8.go
@@ -2,29 +2,16 @@ package sql
 
 func migrateToV8(db *DB) error {
 	if db.Driver == Sqlite3 {
-		if _, err := db.Exec("PRAGMA foreign_keys=OFF;"); err != nil {
-			return err
-		}
+		db.DB.MustExec("PRAGMA foreign_keys=OFF;")
 	}
+
 	tx := db.MustBegin()
 	tx.MustExec(db.SQL("v8/drop_account_name_unique_constraint"))
 	tx.MustExec(`UPDATE schema SET version = 8;`)
 	err := tx.Commit()
 
 	if db.Driver == Sqlite3 {
-		if _, fkErr := db.Exec("PRAGMA foreign_keys=ON;"); fkErr != nil {
-			if err != nil {
-				return err
-			}
-			return fkErr
-		}
-
-		if _, fkErr := db.Queryx("PRAGMA foreign_key_check;"); fkErr != nil {
-			if err != nil {
-				return err
-			}
-			return fkErr
-		}
+		db.DB.MustExec("PRAGMA foreign_keys=ON;")
 	}
 
 	return err
