6ed8d0420caa8fafe811c20cca9f3e628819af7e
Andrew Snodgrass <asnodgrass@anypresence.com>
2015-12-21 09:58:23 -0700

    [#110440118] Added html emails from marketing

--- src/gateway/mail/confirm.go
+++ src/gateway/mail/confirm.go
@@ -6,32 +6,6 @@ import (
 	apsql "gateway/sql"
 )
 
-const confirmTemplate = `{{define "body"}}
-  {{if .Resend}}
-	<p>
-	  <b>
-		  You tried to register an already registered but unconfirmed account.
-			Upon confirming, please use the first password you registered with.
-			If you forget that, please choose the password reset link.
-		</b>
-	</p>
-	{{end}}
-  <p>Thanks for your interest in JustAPIs.</p>
-	<p>
-	  Click on the link below to confirm your email:<br/>
-    <a href="{{.UrlPrefix}}confirmation?token={{.Token}}">confirm email</a>
-	</p>
-	<p>
-	  If you have any questions or require support, please contact
-	  <a href="mailto:support@anypresence.com">support@anypresence.com</a>
-	</p>
-	<p>
-	  Thank you<br/>
-    -The AnyPresence Team
-	</p>
-{{end}}
-`
-
 func SendConfirmEmail(_smtp config.SMTP, proxyServer config.ProxyServer, admin config.ProxyAdmin,
 	user *model.User, tx *apsql.Tx, async bool) error {
 	token, resend := user.Token, true
@@ -48,7 +22,7 @@ func SendConfirmEmail(_smtp config.SMTP, proxyServer config.ProxyServer, admin c
 	context.Subject = "JustAPIs Email Confirmation"
 	context.Token = token
 	context.Resend = resend
-	err := Send(confirmTemplate, context, _smtp, user, async)
+	err := Send("confirm.html", context, _smtp, user, async)
 	if err != nil {
 		return err
 	}
--- src/gateway/mail/mail.go
+++ src/gateway/mail/mail.go
@@ -11,32 +11,6 @@ import (
 	"gateway/model"
 )
 
-const mailTemplate = `From: {{.From}}
-To: {{.To}}
-Subject: {{.Subject}}
-MIME-version: 1.0;
-Content-Type: text/html; charset="UTF-8";
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>{{.Subject}}</title>
-	<style type="text/css">
-	 body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
-	 ul li {
-    color: #386EFF;
-	 }
-	 ul li a {
-    color: blue;
-	 }
-	</style>
- </head>
- <body>{{template "body" .}}</body>
-</html>
-`
-
 type EmailTemplate struct {
 	From    string
 	To      string
@@ -100,13 +74,21 @@ func send(email string, _smtp config.SMTP, body []byte) error {
 	return err
 }
 
-func Send(text string, data interface{}, _smtp config.SMTP, user *model.User, async bool) error {
+func Send(bodyTemplate string, data interface{}, _smtp config.SMTP, user *model.User, async bool) error {
 	t := template.New("template")
-	t, err := t.Parse(mailTemplate)
+	html, err := Asset("mail.html")
 	if err != nil {
 		return err
 	}
-	t, err = t.Parse(text)
+	t, err = t.Parse(string(html))
+	if err != nil {
+		return err
+	}
+	html, err = Asset(bodyTemplate)
+	if err != nil {
+		return err
+	}
+	t, err = t.Parse(string(html))
 	if err != nil {
 		return err
 	}
