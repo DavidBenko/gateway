27839d3ca092b645f701ed47db5f364543ca181b
Bodie Solomon <bodie@synapsegarden.net>
2016-01-20 12:43:45 -0500

    [#89625966] Remove unneeded SQL sub-select

--- src/gateway/sql/static/ansi/proxy_endpoint_component_references/all_endpoint.sql
+++ src/gateway/sql/static/ansi/proxy_endpoint_component_references/all_endpoint.sql
@@ -1,15 +1,11 @@
 SELECT pc.id AS id
-  , cr_by_proxy_id.id AS proxy_endpoint_component_reference_id
+  , cr.id AS proxy_endpoint_component_reference_id
   , pc.conditional AS conditional
   , pc.conditional_positive AS conditional_positive
   , pc.type AS type
   , pc.data AS data
   , pc.type_discriminator AS type_discriminator
-FROM proxy_endpoint_components AS pc,
-  (SELECT cr.id
-    , cr.proxy_endpoint_component_id
-    , cr.position
-    FROM proxy_endpoint_component_references AS cr
-    WHERE cr.proxy_endpoint_id = ?) AS cr_by_proxy_id
-WHERE pc.id = cr_by_proxy_id.proxy_endpoint_component_id
-ORDER BY cr_by_proxy_id.position ASC;
+FROM proxy_endpoint_components pc, proxy_endpoint_component_references cr
+WHERE pc.id = cr.proxy_endpoint_component_id
+  AND cr.proxy_endpoint_id = ?
+ORDER BY cr.position ASC;
