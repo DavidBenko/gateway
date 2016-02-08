d07c8a66128789e19f6a45bfd52ba34ecaaae39d
Andrew Snodgrass <asnodgrass@anypresence.com>
2016-02-08 10:47:57 -0700

    [#110075726] Added scratch pad test endpoint

--- src/gateway/admin/scratch_pads.go
+++ src/gateway/admin/scratch_pads.go
@@ -1,11 +1,13 @@
 package admin
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"gateway/config"
 	"gateway/core"
 	aphttp "gateway/http"
+	"gateway/model"
 	apsql "gateway/sql"
 
 	"github.com/gorilla/handlers"
@@ -31,6 +33,66 @@ func RouteScratchPads(controller ResourceController, path string,
 	router.Handle(path+"/{id}/test", handlers.MethodHandler(routes))
 }
 
+type ScratchPadResult struct {
+	Request  string `json:"request"`
+	Response string `json:"response"`
+}
+
 func (c *MetaScratchPadsController) Test(w http.ResponseWriter, r *http.Request, db *apsql.DB) aphttp.Error {
+	object := model.ScratchPad{}
+	c.mapFields(r, &object)
+	pad, err := object.Find(db)
+	if err != nil {
+		return c.notFound()
+	}
+
+	endpoint, err := model.FindRemoteEndpointForAPIIDAndAccountID(db, pad.RemoteEndpointID, pad.APIID, pad.AccountID)
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+	for _, data := range endpoint.EnvironmentData {
+		if data.ID == pad.RemoteEndpointEnvironmentDataID {
+			endpoint.SelectedEnvironmentData = &data.Data
+			break
+		}
+	}
+
+	vm := core.VMCopy()
+	_, err = vm.Run(pad.Code)
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+
+	result := ScratchPadResult{}
+	obj, err := vm.Get("test")
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+	result.Request = obj.String()
+	var rawRequest json.RawMessage
+	err = json.Unmarshal([]byte(result.Request), &rawRequest)
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+
+	request, err := c.PrepareRequest(endpoint, &rawRequest)
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+	response := request.Perform()
+	jsonResponse, err := response.JSON()
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+	result.Response = string(jsonResponse)
+
+	body, err := json.MarshalIndent(&result, "", "    ")
+	if err != nil {
+		return aphttp.NewError(err, http.StatusBadRequest)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
+
 	return nil
 }
