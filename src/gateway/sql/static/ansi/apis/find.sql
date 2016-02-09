a4e8feb0f686b73489c864ea983579b102049811
Rick Snyder <rsnyder@anypresence.com>
2016-02-09 15:19:48 -0500

    [#113238189] Populate AccountID field when finding APIs

--- src/gateway/sql/static/ansi/apis/find.sql
+++ src/gateway/sql/static/ansi/apis/find.sql
@@ -1,7 +1,8 @@
 SELECT
   id, name, description,
   cors_allow_origin, cors_allow_headers, cors_allow_credentials,
-  cors_request_headers, cors_max_age, enable_swagger
+  cors_request_headers, cors_max_age, enable_swagger,
+  account_id
 FROM apis
 WHERE id = ?
   AND account_id = ?;
