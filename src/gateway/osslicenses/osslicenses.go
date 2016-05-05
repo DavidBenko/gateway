fa02c5ba3499da4546431cc571fccf95f3132ed3
Jeffrey Bozek <jbozek@anypresence.com>
2016-05-05 10:35:31 -0400

    Correct formatting.

---
+++ src/gateway/osslicenses/osslicenses.go
@@ -0,0 +1,30 @@
+package osslicenses
+
+// OssLicenseList lists all used libraries and associated OSS Licenses.
+const OssLicenseList = `The following software is used by and incorporated into this product:
+
+github.com/aymerick/raymond (MIT)
+github.com/blevesearch/bleve (Apache 2.0)
+github.com/gorilla/context (New BSD)
+github.com/gorilla/handlers (unknown)
+github.com/gorilla/mux (New BSD)
+github.com/gorilla/sessions (New BSD)
+github.com/jmoiron/sqlx (MIT)
+github.com/mattbaird/elastigo (Apache 2.0)
+github.com/vincent-petithory/dataurl (MIT)
+golang.org/x/net (New BSD)
+github.com/BurntSushi/toml (unknown)
+github.com/robertkrimen/otto (MIT)
+github.com/denisenkom/go-mssqldb (New BSD)
+github.com/go-ldap/ldap (New BSD)
+gopkg.in/mgo.v2/bson (unknown)
+github.com/go-sql-driver/mysql (Mozilla Public License 2.0)
+github.com/jackc/pgx (MIT)
+gopkg.in/airbrake/gobrake.v2 (New BSD)
+github.com/xeipuuv/gojsonschema (Apache 2.0, MIT)
+golang.org/x/crypto (New BSD)
+github.com/gdamore/mangos (Apache 2.0)
+github.com/lib/pq (MIT)
+github.com/mattn/go-sqlite3 (MIT)
+github.com/boltdb/bolt (MIT)
+`
