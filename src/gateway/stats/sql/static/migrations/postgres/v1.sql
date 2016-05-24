c31fa0b5b3b9984df9f3924ae6f019648a732325
Bodie Solomon <bodie@synapsegarden.net>
2016-05-24 15:56:19 -0400

    [#101492056] Add stats/sql migrations for all vars

--- src/gateway/stats/sql/static/migrations/postgres/v1.sql
+++ src/gateway/stats/sql/static/migrations/postgres/v1.sql
@@ -2,12 +2,24 @@ CREATE TABLE "stats" (
   "id" SERIAL PRIMARY KEY
   , "node" TEXT NOT NULL
   , "ms" BIGINT NOT NULL
-  , "api_id" INT NOT NULL
   , "request_size" INT NOT NULL
   , "request_id" TEXT NOT NULL
+  , "api_id" INT NOT NULL
+  , "api_name" TEXT NOT NULL
+  , "host_id" INT NOT NULL
+  , "host_name" TEXT NOT NULL
+  , "proxy_id" INT NOT NULL
+  , "proxy_name" TEXT NOT NULL
+  , "proxy_env_id" INT NOT NULL
+  , "proxy_env_name" TEXT NOT NULL
+  , "proxy_route_path" TEXT NOT NULL
+  , "proxy_route_verb" TEXT NOT NULL
+  , "proxy_group_id" INT NOT NULL
+  , "proxy_group_name" TEXT NOT NULL
   , "response_time" INT NOT NULL
   , "response_size" INT NOT NULL
   , "response_status" INT NOT NULL
   , "response_error" TEXT NOT NULL
+  , "remote_endpoint_response_time" INT NOT NULL
   , "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL
 )
