b3fd549ab5a9e10ff7de32127857a8aa9713e94f
Nass <nasali@anypresence.com>
2016-06-03 15:46:12 -0600

    Add Oracle spec and test. [#120452409]

---
+++ src/gateway/db/sql/oracle.go
@@ -0,0 +1,65 @@
+package sql
+
+import (
+	"errors"
+	"fmt"
+
+	"gateway/db"
+
+	_ "github.com/mattn/go-oci8"
+)
+
+// OracleSpec implements db.Specifier for Oracle connection parameters.
+type OracleSpec struct {
+	spec
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DbName   string `json:"dbname"`
+	Host     string `json:"host"`
+}
+
+func (o *OracleSpec) validate() error {
+	return validate(o, []validation{
+		{kw: "port", errCond: o.Port < 0, val: o.Port},
+		{kw: "user", errCond: o.User == "", val: o.User},
+		{kw: "password", errCond: o.Password == "", val: o.Password},
+		{kw: "dbname", errCond: o.DbName == "", val: o.DbName},
+		{kw: "host", errCond: o.Host == "", val: o.Host},
+	})
+}
+
+func (o *OracleSpec) driver() driver {
+	return oracle
+}
+
+func (o *OracleSpec) ConnectionString() string {
+	return fmt.Sprintf("oci8://%s:%s@%s:%d/%s",
+		o.User,
+		o.Password,
+		o.Host,
+		o.Port,
+		o.DbName,
+	)
+}
+
+func (o *OracleSpec) UniqueServer() string {
+	return o.ConnectionString()
+}
+
+func (o *OracleSpec) NewDB() (db.DB, error) {
+	return newDB(o)
+}
+
+// UpdateWith validates `oSpec` and updates `o` with its contents if it is
+// valid.
+func (o *OracleSpec) UpdateWith(oSpec *OracleSpec) error {
+	if oSpec == nil {
+		return errors.New("cannot update an OracleSpec with a nil Specifier")
+	}
+	if err := oSpec.validate(); err != nil {
+		return err
+	}
+	*o = *oSpec
+	return nil
+}
---
+++ src/gateway/db/sql/oracle_test.go
@@ -0,0 +1,74 @@
+package sql_test
+
+import (
+	"encoding/json"
+
+	sql "gateway/db/sql"
+
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+)
+
+func OracleConfigs() map[string]map[string]interface{} {
+	return map[string]map[string]interface{}{
+		"simple": map[string]interface{}{
+			"host":     "database.oracle.net",
+			"port":     1521,
+			"user":     "scott",
+			"password": "tiger",
+			"dbname":   "XE",
+		},
+		"bad": map[string]interface{}{
+			"port":   -1234,
+			"user":   "user",
+			"dbname": "db",
+		},
+	}
+}
+
+func oracleSpecs() map[string]*sql.OracleSpec {
+	specs := make(map[string]*sql.OracleSpec)
+	for name, spc := range OracleConfigs() {
+		oracleSpec := &sql.OracleSpec{}
+		vals, err := json.Marshal(spc)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(vals, oracleSpec)
+
+		specs[name] = oracleSpec
+	}
+	return specs
+}
+
+func (s *SQLSuite) TestPostgresConfig(c *gc.C) {
+	for i, t := range []struct {
+		should       string
+		given        *sql.OracleSpec
+		expectString string
+		expectUnique string
+		expectError  string
+	}{{
+		should:       "work with a simple config",
+		given:        oracleSpecs()["simple"],
+		expectString: "oci8://scott:tiger@database.oracle.net:1521/XE",
+		expectUnique: "oci8://scott:tiger@database.oracle.net:1521/XE",
+	}, {
+		should: "fail with multiple bad config items",
+		given:  oracleSpecs()["bad"],
+		expectError: `pgx config errors: ` +
+			`bad value "" for "password"; ` +
+			`bad value "" for "host"; `,
+	}} {
+		c.Logf("Test %d: should %s", i, t.should)
+
+		obtained, err := sql.Config(sql.Connection(t.given))
+		if t.expectError != "" {
+			c.Check(err, gc.ErrorMatches, t.expectError)
+			continue
+		}
+		c.Assert(err, jc.ErrorIsNil)
+		c.Check(obtained.ConnectionString(), gc.Equals, t.expectString)
+		c.Check(obtained.UniqueServer(), gc.Equals, t.expectUnique)
+	}
+}
