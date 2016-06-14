04ddaaca3ffb718fbffd98ba1ae4d609720cb58a
Jeremiah Piontek <jwpiontek@gmail.com>
2016-06-14 16:15:17 -0500

    [#112846011] Added redis remote endpoint model

---
+++ src/gateway/model/remote_endpoint/redis.go
@@ -0,0 +1,36 @@
+package remote_endpoints
+
+import (
+	"encoding/json"
+	"fmt"
+	"gateway/db"
+	"gateway/db/redis"
+
+	"github.com/jmoiron/sqlx/types"
+)
+
+type Redis struct {
+	Config    *redis.Config `json:"config"`
+	MaxActive int           `json:"maxActive"`
+	MaxIdle   int           `json:"maxIdle"`
+}
+
+func RedisConfig(data types.JsonText) (db.Specifier, error) {
+	var conf Redis
+	err := json.Unmarshal(data, &conf)
+	if err != nil {
+		return nil, fmt.Errorf("bad JSON for Redis config: %s", err.Error())
+	}
+
+	spec, err := redis.Config(
+		redis.Connection(conf),
+		redis.MaxActive(conf.MaxActive),
+		redis.MaxIdle(conf.MaxIdle),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return spec, nil
+}
