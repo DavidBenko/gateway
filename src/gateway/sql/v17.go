1460466d06c2be24cc2f86a3e45f8ad5ae323c98
Andrew Snodgrass <pointlander@gmail.com>
2016-08-26 13:35:01 -0600

    [#114493225] Simplify migration

--- src/gateway/sql/v17.go
+++ src/gateway/sql/v17.go
@@ -1,14 +1,11 @@
 package sql
 
 func migrateToV17(db *DB) error {
-	db.MustExec(`PRAGMA foreign_keys = OFF;`)
+	db.DisableSqliteTriggers()
+	defer db.EnableSqliteTriggers()
+
 	tx := db.MustBegin()
 	tx.MustExec(db.SQL("v17/create_job_fields"))
 	tx.MustExec(`UPDATE schema SET version = 17;`)
-	err := tx.Commit()
-	if err != nil {
-		return err
-	}
-	db.MustExec(`PRAGMA foreign_keys = ON;`)
-	return nil
+	return tx.Commit()
 }
