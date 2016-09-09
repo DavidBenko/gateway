217a12b544e614a7d707467f162f58a6fa398137
Nass <nasali@anypresence.com>
2016-09-09 13:09:09 -0600

    Add generate directive for Sample to controller.

--- src/gateway/admin/controller.go
+++ src/gateway/admin/controller.go
@@ -27,6 +27,7 @@ import (
 //go:generate ./controller.rb --model PushMessage --reflect
 //go:generate ./controller.rb --model PushChannelMessage --reflect
 //go:generate ./controller.rb --model Plan --allow-create=false --allow-update=false --allow-delete=false
+//go:generate ./controller.rb --model Sample --account --before-validate-hook
 
 // ResourceController defines what we expect a controller to do to route
 // a RESTful resource
