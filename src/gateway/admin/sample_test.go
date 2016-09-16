880fa1c4269b653b5a15a178d1559c7c73876cb5
Nass <nasali@anypresence.com>
2016-09-16 09:48:07 -0600

    Refactor sample_test to loop over tests.

--- src/gateway/admin/sample_test.go
+++ src/gateway/admin/sample_test.go
@@ -14,17 +14,35 @@ func (a *AdminSuite) TestSampleBeforeValidate(c *gc.C) {
 	user1 := testing.PrepareUser(c, a.db, acc1.ID, testing.JeffUser)
 	testing.PrepareAPI(c, a.db, acc1.ID, user1.ID, testing.API2)
 	controller := &admin.SamplesController{}
-	given := &model.Sample{
-		Name:        "Sample1",
-		Constraints: []stats.Constraint{{Key: "api.id", Operator: "foo", Value: int64(5)}},
-		AccountID:   acc1.ID,
-		UserID:      user1.ID,
+
+	for i, t := range []struct {
+		should      string
+		given       *model.Sample
+		expect      string
+		expectError string
+	}{{
+		should: "give error for invalid Operator",
+		given: &model.Sample{
+			Name:      "Sample1",
+			AccountID: acc1.ID,
+			UserID:    user1.ID,
+			Constraints: []stats.Constraint{
+				{Key: "api.id", Operator: "foo", Value: int64(5)},
+			},
+		},
+		expectError: `invalid operator "foo" for single api.id value, use "EQ"`,
+	}} {
+		c.Logf("test %d: should %s", i, t.should)
+
+		tx, err := a.db.Begin()
+		c.Assert(err, gc.IsNil)
+		gotten := controller.BeforeValidate(t.given, tx)
+		if t.expectError != "" {
+			c.Check(gotten, gc.ErrorMatches, t.expectError)
+		} else {
+			c.Check(gotten, gc.DeepEquals, t.expect)
+		}
+		tx.Commit()
+
 	}
-	expected := `invalid operator "foo" for single api.id value, use "EQ"`
-	c.Logf("test 0: should %s", "give error for invalid Operator")
-	tx, err := a.db.Begin()
-	c.Assert(err, gc.IsNil)
-	gotten := controller.BeforeValidate(given, tx)
-	c.Check(gotten, gc.ErrorMatches, expected)
-	tx.Commit()
 }
