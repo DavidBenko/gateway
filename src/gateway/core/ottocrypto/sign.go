0342e8f412bda3abd88e99765f400fbbcb2997e4
Jeremiah Piontek <jwpiontek@gmail.com>
2016-11-11 12:53:38 -0600

    Use getData func

--- src/gateway/core/ottocrypto/sign.go
+++ src/gateway/core/ottocrypto/sign.go
@@ -26,16 +26,9 @@ func IncludeSigning(vm *otto.Otto, accountID int64, keySource KeyDataSource) {
 
 func setSign(vm *otto.Otto, accountID int64, keySource KeyDataSource) {
 	vm.Set("_sign", func(call otto.FunctionCall) otto.Value {
-		d, err := getArgument(call, 0)
-
+		data, err := getData(call)
 		if err != nil {
 			logreport.Println(err)
-			return undefined
-		}
-
-		data, ok := d.(string)
-		if !ok {
-			logreport.Println("data should be a string")
 			return undefined
 		}
 
@@ -88,16 +81,9 @@ func setSign(vm *otto.Otto, accountID int64, keySource KeyDataSource) {
 
 func setVerify(vm *otto.Otto, accountID int64, keySource KeyDataSource) {
 	vm.Set("_verify", func(call otto.FunctionCall) otto.Value {
-		d, err := getArgument(call, 0)
-
+		data, err := getData(call)
 		if err != nil {
 			logreport.Println(err)
-			return undefined
-		}
-
-		data, ok := d.(string)
-		if !ok {
-			logreport.Println("data should be a string")
 			return undefined
 		}
 
