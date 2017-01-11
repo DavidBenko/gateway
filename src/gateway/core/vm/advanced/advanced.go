9c5da7403a2d62986ad1f35d866c92b28ac888b1
Jeremiah Piontek <jwpiontek@gmail.com>
2017-01-11 15:36:06 -0600

    Better concurrency support for different endpoint types

--- src/gateway/core/vm/advanced/advanced.go
+++ src/gateway/core/vm/advanced/advanced.go
@@ -2,6 +2,7 @@ package advanced
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"gateway/core/request"
 	corevm "gateway/core/vm"
@@ -19,60 +20,22 @@ func IncludePerform(vm *otto.Otto, accountID int64, endpointSource corevm.DataSo
 	vm.Set("_perform", func(call otto.FunctionCall) otto.Value {
 		connections := make(map[int64]io.Closer)
 
-		c, err := corevm.GetArgument(call, 0)
+		r, err := corevm.GetArgument(call, 0)
 		if err != nil {
-			return corevm.OttoErrorObject(vm, "missing codename argument")
+			return corevm.OttoErrorObject(vm, "invalid argument")
 		}
 
-		if _, ok := c.(string); !ok {
-			return corevm.OttoErrorObject(vm, "codename should be a string")
-		}
-		codename := c.(string)
-
-		r, err := call.Argument(1).Export()
-		if r == nil {
-			return corevm.OttoErrorObject(vm, "missing request argument")
-		}
+		results, _, err := perform(r, vm, endpointSource, accountID, prepare, connections)
 		if err != nil {
 			return corevm.OttoErrorObject(vm, err.Error())
 		}
 
-		switch r.(type) {
-		case []map[string]interface{}:
-			responses, err := performMultiple(r.([]map[string]interface{}), vm, endpointSource, accountID, codename, prepare, connections)
-			if err != nil {
-				return corevm.OttoErrorObject(vm, err.Error())
-			}
-
-			if len(responses) == 0 {
-				return otto.UndefinedValue()
-			}
-
-			json := ""
-			for _, response := range responses {
-				rj, err := response.JSON()
-				if err != nil {
-					corevm.OttoErrorObject(vm, fmt.Sprintf("could not convert response to Javascript object: %s", err.Error()))
-				}
-				json += fmt.Sprintf("%s,", rj)
-			}
-
-			json = fmt.Sprintf("[%s]", json)
-			return corevm.ToOttoObjectValue(vm, json)
-		case map[string]interface{}:
-			response, err := perform(r.(map[string]interface{}), vm, endpointSource, accountID, codename, prepare, connections)
-			if err != nil {
-				return corevm.OttoErrorObject(vm, err.Error())
-			}
-
-			b, err := response.JSON()
-			if err != nil {
-				return corevm.OttoErrorObject(vm, err.Error())
-			}
-			return corevm.ToOttoObjectValue(vm, string(b))
-		default:
-			return corevm.OttoErrorObject(vm, "invalid request object")
+		sr, err := json.Marshal(results)
+		if err != nil {
+			return corevm.OttoErrorObject(vm, err.Error())
 		}
+
+		return corevm.ToOttoObjectValue(vm, string(sr))
 	})
 
 	scripts := []string{
@@ -85,39 +48,142 @@ func IncludePerform(vm *otto.Otto, accountID int64, endpointSource corevm.DataSo
 	}
 }
 
-func perform(r interface{}, vm *otto.Otto, endpointSource corevm.DataSource, accountID int64,
-	codename string, prepare RequestPreparer, connections map[int64]io.Closer) (request.Response, error) {
-
-	rs, err := json.Marshal(r)
+func parseOptions(o map[string]interface{}) (*requestOptions, error) {
+	jsonOptions, err := json.Marshal(o)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("unable to parse options object")
 	}
 
-	var raw *json.RawMessage
-	err = json.Unmarshal(rs, &raw)
+	var options *requestOptions
+	err = json.Unmarshal(jsonOptions, &options)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("invalid request options object")
 	}
 
-	criteria := &corevm.RemoteEndpointStoreCriteria{AccountID: accountID, Codename: codename}
+	// Create json.RawMessage from the requests
+	options.rawRequests = make([]*json.RawMessage, len(options.Requests))
+	for i, request := range options.Requests {
+		r, err := json.Marshal(request)
+		if err != nil {
+			return nil, fmt.Errorf("invalid request at index %d", i)
+		}
+
+		var raw *json.RawMessage
+		err = json.Unmarshal(r, &raw)
+		if err != nil {
+			return nil, err
+		}
+
+		options.rawRequests[i] = raw
+	}
+
+	return options, nil
+}
+
+func perform(o interface{}, vm *otto.Otto, endpointSource corevm.DataSource, accountID int64,
+	prepare RequestPreparer, connections map[int64]io.Closer) (interface{}, string, error) {
+
+	switch o.(type) {
+	case map[string]interface{}:
+		// single endpoint
+		options, err := parseOptions(o.(map[string]interface{}))
+		if err != nil {
+			return nil, "", err
+		}
+
+		results := makeRequests(options, accountID, endpointSource, prepare, connections)
+
+		responses := make(map[string][]request.Response, 1)
+		responses[options.Codename] = results
+
+		return responses, options.Codename, nil
+	case []map[string]interface{}:
+		// multiple endpoints
+		allopts := o.([]map[string]interface{})
+		c := make(chan *responsesWrapper, len(allopts))
+
+		for i, opt := range allopts {
+			go func(index int, options map[string]interface{}) {
+				responses, codename, err := perform(options, vm, endpointSource, accountID, prepare, connections)
+				if err != nil {
+					res := &errorResponse{err.Error()}
+					c <- &responsesWrapper{"", []request.Response{res}}
+				} else {
+					c <- &responsesWrapper{codename, responses.(map[string][]request.Response)[codename]}
+				}
+			}(i, opt)
+		}
+
+		responses := make(map[string][]request.Response, len(allopts))
+		for i := 0; i < len(allopts); i++ {
+			select {
+			case result := <-c:
+				if result != nil {
+					if val, ok := responses[result.codename]; ok {
+						responses[result.codename] = append(val, result.responses...)
+					} else {
+						responses[result.codename] = result.responses
+					}
+				}
+			}
+		}
+
+		return responses, "", nil
+	default:
+		return nil, "", errors.New("invalid argument type")
+	}
+}
+
+func makeRequests(options *requestOptions, accountID int64, endpointSource corevm.DataSource,
+	prepare RequestPreparer, connections map[int64]io.Closer) []request.Response {
+
+	criteria := &corevm.RemoteEndpointStoreCriteria{AccountID: accountID, Codename: options.Codename}
 	result, ok := endpointSource.Get(criteria)
 	if !ok {
-		return nil, fmt.Errorf("could not find remote endpoint %s", codename)
+		return []request.Response{&errorResponse{fmt.Sprintf("could not find remote endpoint %s", options.Codename)}}
 	}
 
 	endpoint := result.(*model.RemoteEndpoint)
 
-	req, err := prepare(endpoint, raw, connections)
-	if err != nil {
-		return nil, err
+	responses := make([]request.Response, len(options.rawRequests))
+
+	c := make(chan *responseWrapper, len(options.rawRequests))
+	for i, r := range options.rawRequests {
+		go func(index int, req *json.RawMessage) {
+			preparedRequest, err := prepare(endpoint, req, connections)
+			if err != nil {
+				e := &errorResponse{err.Error()}
+				c <- &responseWrapper{index, e}
+			} else {
+				c <- &responseWrapper{index, preparedRequest.Perform()}
+			}
+		}(i, r)
 	}
 
-	return req.Perform(), nil
+	for i := 0; i < len(options.rawRequests); i++ {
+		select {
+		case response := <-c:
+			responses[response.index] = response.response
+		}
+	}
+
+	return responses
 }
 
-type payload struct {
+type requestOptions struct {
+	Codename    string                   `json:"codename"`
+	Requests    []map[string]interface{} `json:"requests"`
+	rawRequests []*json.RawMessage
+}
+
+type responseWrapper struct {
 	index    int
 	response request.Response
+}
+
+type responsesWrapper struct {
+	codename  string
+	responses []request.Response
 }
 
 // errorResponse is used to wrap an error that may have occurred before the actual
@@ -126,38 +192,12 @@ type errorResponse struct {
 	Error string `json:"error"`
 }
 
+// JSON satisfies the request.Response interface
 func (e *errorResponse) JSON() ([]byte, error) {
 	return json.Marshal(e)
 }
 
+// Log satisfies the request.Response interface
 func (e *errorResponse) Log() string {
 	return ""
-}
-
-func performMultiple(r []map[string]interface{}, vm *otto.Otto, endpointSource corevm.DataSource, accountID int64,
-	codename string, prepare RequestPreparer, connections map[int64]io.Closer) ([]request.Response, error) {
-
-	responses := make([]request.Response, len(r))
-	c := make(chan *payload, len(r))
-
-	for i, req := range r {
-		go func(index int, req interface{}) {
-			res, err := perform(req, vm, endpointSource, accountID, codename, prepare, connections)
-			if err != nil {
-				// return an error response for the consumer to handle
-				errResponse := &errorResponse{err.Error()}
-				c <- &payload{index, errResponse}
-			}
-			c <- &payload{index, res}
-		}(i, req)
-	}
-
-	for i := 0; i < len(r); i++ {
-		select {
-		case req := <-c:
-			responses[req.index] = req.response
-		}
-	}
-
-	return responses, nil
 }
